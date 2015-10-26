fe19f86985bf072885b4280aaa37138a65cf7cd3
hlomzik <hlomzik@gmail.com>
2015-10-26 19:38:33 +0300

    Reformat the code to have a style :)

--- odauth.js
+++ odauth.js
@@ -47,153 +47,150 @@ class OneDriveAuth {
     }
     this.appInfo = appInfo;
   }
-auth(wasClicked) {
-  ensureHttps();
-  var token = this.getTokenFromCookie();
-  if (token) {
-    window.onAuthenticated(token);
+  
+  auth(wasClicked) {
+    ensureHttps();
+    var token = this.getTokenFromCookie();
+    if (token) {
+      window.onAuthenticated(token);
+    } else if (wasClicked) {
+      this.challengeForAuth();
+    } else {
+      this.showLoginButton();
+    }
   }
-  else if (wasClicked) {
-    this.challengeForAuth();
-  }
-  else {
-    this.showLoginButton();
-  }
-}
 
 // for added security we require https
-ensureHttps() {
-  if (window.location.protocol != "https:") {
-    window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
-  }
-}
-
-static onAuthCallback() {
-  var authInfo = this.getAuthInfoFromUrl();
-  var token = authInfo["access_token"];
-  var expiry = parseInt(authInfo["expires_in"]);
-  this.setCookie(token, expiry);
-  window.opener.onAuthenticated(token, window);
-}
-
-static getAuthInfoFromUrl() {
-  if (window.location.hash) {
-    var authResponse = window.location.hash.substring(1);
-    var authInfo = JSON.parse(
-      '{"' + authResponse.replace(/&/g, '","').replace(/=/g, '":"') + '"}',
-      function(key, value) { return key === "" ? value : decodeURIComponent(value); });
-    return authInfo;
-  }
-  else {
-    alert("failed to receive auth token");
-  }
-}
-
-getTokenFromCookie() {
-  var cookies = document.cookie;
-  var name = "odauth=";
-  var start = cookies.indexOf(name);
-  if (start >= 0) {
-    start += name.length;
-    var end = cookies.indexOf(';', start);
-    if (end < 0) {
-      end = cookies.length;
+  ensureHttps() {
+    if (window.location.protocol != "https:") {
+      window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
     }
-    else {
-      var postCookie = cookies.substring(end);
+  }
+  
+  static onAuthCallback() {
+    var authInfo = this.getAuthInfoFromUrl();
+    var token = authInfo["access_token"];
+    var expiry = parseInt(authInfo["expires_in"]);
+    this.setCookie(token, expiry);
+    window.opener.onAuthenticated(token, window);
+  }
+  
+  static getAuthInfoFromUrl() {
+    if (window.location.hash) {
+      var authResponse = window.location.hash.substring(1);
+      var authInfo = JSON.parse(
+        '{"' + authResponse.replace(/&/g, '","').replace(/=/g, '":"') + '"}',
+        (key, value) => key === "" ? value : decodeURIComponent(value)
+      );
+      return authInfo;
+    } else {
+      alert("failed to receive auth token");
     }
-
-    var value = cookies.substring(start, end);
-    return value;
   }
-
-  return "";
-}
-
-static setCookie(token, expiresInSeconds) {
-  var expiration = new Date();
-  expiration.setTime(expiration.getTime() + expiresInSeconds * 1000);
-  var cookie = "odauth=" + token +"; path=/; expires=" + expiration.toUTCString();
-
-  if (document.location.protocol.toLowerCase() == "https") {
-    cookie = cookie + ";secure";
+  
+  getTokenFromCookie() {
+    var cookies = document.cookie;
+    var name = "odauth=";
+    var start = cookies.indexOf(name);
+    if (start >= 0) {
+      start += name.length;
+      var end = cookies.indexOf(';', start);
+      if (end < 0) {
+        end = cookies.length;
+      }
+      
+      var value = cookies.substring(start, end);
+      return value;
+    }
+    
+    return "";
   }
-
-  document.cookie = cookie;
-}
+  
+  static setCookie(token, expiresInSeconds) {
+    var expiration = new Date();
+    expiration.setTime(expiration.getTime() + expiresInSeconds * 1000);
+    var cookie = "odauth=" + token + "; path=/; expires=" + expiration.toUTCString();
+    
+    if (document.location.protocol.toLowerCase() == "https") {
+      cookie = cookie + ";secure";
+    }
+    
+    document.cookie = cookie;
+  }
 
 // called when a login button needs to be displayed for the user to click on.
 // if a customLoginButton() function is defined by your app, it will be called
 // with 'true' passed in to indicate the button should be added. otherwise, it
 // will insert a textual login link at the top of the page. if defined, your
 // showCustomLoginButton should call challengeForAuth() when clicked.
-showLoginButton() {
-  if (typeof showCustomLoginButton === "function") {
-    showCustomLoginButton(true);
-    return;
+  showLoginButton() {
+    if (typeof window.showCustomLoginButton === "function") {
+      window.showCustomLoginButton(true);
+      return;
+    }
+    
+    var loginText = document.createElement('a');
+    loginText.href = "#";
+    loginText.id = "loginText";
+    loginText.onclick = this.challengeForAuth.bind(this);
+    loginText.innerText = "[sign in]";
+    document.body.insertBefore(loginText, document.body.children[0]);
   }
-
-  var loginText = document.createElement('a');
-  loginText.href = "#";
-  loginText.id = "loginText";
-  loginText.onclick = this.challengeForAuth.bind(this);
-  loginText.innerText = "[sign in]";
-  document.body.insertBefore(loginText, document.body.children[0]);
-}
 
 // called with the login button created by showLoginButton() needs to be
 // removed. if a customLoginButton() function is defined by your app, it will
 // be called with 'false' passed in to indicate the button should be removed.
 // otherwise it will remove the textual link that showLoginButton() created.
-removeLoginButton() {
-  if (typeof showCustomLoginButton === "function") {
-    showCustomLoginButton(false);
-    return;
+  removeLoginButton() {
+    if (typeof window.showCustomLoginButton === "function") {
+      window.showCustomLoginButton(false);
+      return;
+    }
+    
+    var loginText = document.getElementById("loginText");
+    if (loginText) {
+      document.body.removeChild(loginText);
+    }
   }
-
-  var loginText = document.getElementById("loginText");
-  if (loginText) {
-    document.body.removeChild(loginText);
+  
+  challengeForAuth() {
+    var appInfo = this.appInfo;
+    var url =
+      "https://login.live.com/oauth20_authorize.srf" +
+      "?client_id=" + appInfo.clientId +
+      "&scope=" + encodeURIComponent(appInfo.scopes) +
+      "&response_type=token" +
+      "&redirect_uri=" + encodeURIComponent(appInfo.redirectUri);
+    this.popup(url);
   }
-}
-
-challengeForAuth() {
-  var appInfo = this.appInfo;
-  var url =
-    "https://login.live.com/oauth20_authorize.srf" +
-    "?client_id=" + appInfo.clientId +
-    "&scope=" + encodeURIComponent(appInfo.scopes) +
-    "&response_type=token" +
-    "&redirect_uri=" + encodeURIComponent(appInfo.redirectUri);
-  this.popup(url);
-}
-
-popup(url) {
-  var width = 525,
+  
+  popup(url) {
+    var width = 525,
       height = 525,
       screenX = window.screenX,
       screenY = window.screenY,
       outerWidth = window.outerWidth,
       outerHeight = window.outerHeight;
-
-  var left = screenX + Math.max(outerWidth - width, 0) / 2;
-  var top = screenY + Math.max(outerHeight - height, 0) / 2;
-
-  var features = [
-              "width=" + width,
-              "height=" + height,
-              "top=" + top,
-              "left=" + left,
-              "status=no",
-              "resizable=yes",
-              "toolbar=no",
-              "menubar=no",
-              "scrollbars=yes"];
-  var popup = window.open(url, "oauth", features.join(","));
-  if (!popup) {
-    alert("failed to pop up auth window");
+    
+    var left = screenX + Math.max(outerWidth - width, 0) / 2;
+    var top = screenY + Math.max(outerHeight - height, 0) / 2;
+    
+    var features = [
+      "width=" + width,
+      "height=" + height,
+      "top=" + top,
+      "left=" + left,
+      "status=no",
+      "resizable=yes",
+      "toolbar=no",
+      "menubar=no",
+      "scrollbars=yes"
+    ];
+    var popup = window.open(url, "oauth", features.join(","));
+    if (!popup) {
+      alert("failed to pop up auth window");
+    }
+    
+    popup.focus();
   }
-
-  popup.focus();
-}
 }
