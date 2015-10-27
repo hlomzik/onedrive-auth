f7a05fe7d5ea27525fc94aa6549c153a89137849
hlomzik <hlomzik@gmail.com>
2015-10-27 14:40:17 +0300

    Comment it out properly with JSDoc

--- odauth.js
+++ odauth.js
@@ -1,38 +1,30 @@
-// instructions:
-// - host a copy of callback.html and odauth.js on your domain.
-// - embed odauth.js in your app like this:
-//   <script id="odauth" src="odauth.js"
-//           clientId="YourClientId" scopes="ScopesYouNeed"
-//           redirectUri="YourUrlForCallback.html"></script>
-// - define the onAuthenticated(token) function in your app to receive the auth token.
-// - call odauth() to begin, as well as whenever you need an auth token
-//   to make an API call. if you're making an api call in response to a user's
-//   click action, call odAuth(true), otherwise just call odAuth(). the difference
-//   is that sometimes odauth needs to pop up a window so the user can sign in,
-//   grant your app permission, etc. the pop up can only be launched in response
-//   to a user click, otherwise the browser's popup blocker will block it. when
-//   odauth isn't called in click mode, it'll put a sign-in button at the top of
-//   your page for the user to click. when it's done, it'll remove that button.
-//
-// how it all works:
-// when you call odauth(), we first check if we have the user's auth token stored
-// in a cookie. if so, we read it and immediately call your onAuthenticated() method.
-// if we can't find the auth cookie, we need to pop up a window and send the user
-// to Microsoft Account so that they can sign in or grant your app the permissions
-// it needs. depending on whether or not odauth() was called in response to a user
-// click, it will either pop up the auth window or display a sign-in button for
-// the user to click (which results in the pop-up). when the user finishes the
-// auth flow, the popup window redirects back to your hosted callback.html file,
-// which calls the onAuthCallback() method below. it then sets the auth cookie
-// and calls your app's onAuthenticated() function, passing in the optional 'window'
-// argument for the popup window. your onAuthenticated function should close the
-// popup window if it's passed in.
-//
-// subsequent calls to odauth() will usually complete immediately without the
-// popup because the cookie is still fresh.
 "use strict";
 
+/**
+ * Small lib to go through the OneDrive authentication process and get back the token.
+ * 
+ * Instructions:
+ * - Host a copy of callback.html and odauth.js on your domain.
+ * - Embed odauth.js in your app like this:
+ *   <script src="odauth.js"></script>
+ * - Define the onAuthenticated(token) function in your app to receive the auth token.
+ * - Create instance of OneDriveAuth and call auth() method to begin and whenever you
+ *   need an auth token to make an API call. If you're making an api call in response to
+ *   a user's click action, call auth(true), otherwise just call auth(). The difference
+ *   is that sometimes OneDriveAuth needs to pop up a window so the user can sign in,
+ *   grant your app permission, etc. The pop up can only be launched in response
+ *   to a user click, otherwise the browser's popup blocker will block it. When
+ *   OneDriveAuth isn't called in click mode, it'll put a sign-in button at the top of
+ *   your page for the user to click. When it's done, it'll remove that button.
+ */
 class OneDriveAuth {
+  /**
+   * @constructor
+   * @param {Array} appInfo
+   * @param {string} appInfo.clientId from App Settings
+   * @param {string} appInfo.scopes separated by space ("onedrive.readonly wl.signin" for example)
+   * @param {string} appInfo.redirectUri
+   */
   constructor(appInfo) {
     if (!appInfo.clientId) {
       throw "appInfo object should have `clientId` property set to your application id";
@@ -48,6 +40,25 @@ class OneDriveAuth {
     this.appInfo = appInfo;
   }
   
+  /**
+   * The main auth method. First, we check if we have the user's auth token stored
+   * in a cookie. if so, we read it and immediately call your onAuthenticated() method.
+   * If we can't find the auth cookie, we need to pop up a window and send the user
+   * to Microsoft Account so that they can sign in or grant your app the permissions
+   * it needs. Depending on whether or not auth() was called in response to a user
+   * click, it will either pop up the auth window or display a sign-in button for
+   * the user to click (which results in the pop-up). when the user finishes the
+   * auth flow, the popup window redirects back to your hosted callback.html file,
+   * which calls the onAuthCallback() static method below. It then sets the auth cookie
+   * and calls your app's onAuthenticated() function, passing in the optional 'window'
+   * argument for the popup window. your onAuthenticated function should close the
+   * popup window if it's passed in.
+   *
+   * Subsequent calls to auth() will usually complete immediately without the
+   * popup because the cookie is still fresh.
+   * 
+   * @param {boolean} wasClicked whether the call is result of a click or not
+   */
   auth(wasClicked) {
     ensureHttps();
     var token = this.getTokenFromCookie();
@@ -59,14 +70,21 @@ class OneDriveAuth {
       this.showLoginButton();
     }
   }
-
-// for added security we require https
+  
+  /**
+   * For added security we require https
+   */
   ensureHttps() {
     if (window.location.protocol != "https:") {
       window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
     }
   }
   
+  /**
+   * Called from the callback page after OAuth authorization.
+   * On success it save the token to cookie and call user's onAuthenticated()
+   * function in parent window.
+   */
   static onAuthCallback() {
     var authInfo = this.getAuthInfoFromUrl();
     var token = authInfo["access_token"];
@@ -118,11 +136,13 @@ class OneDriveAuth {
     document.cookie = cookie;
   }
 
-// called when a login button needs to be displayed for the user to click on.
-// if a customLoginButton() function is defined by your app, it will be called
-// with 'true' passed in to indicate the button should be added. otherwise, it
-// will insert a textual login link at the top of the page. if defined, your
-// showCustomLoginButton should call challengeForAuth() when clicked.
+  /**
+   * Called when a login button needs to be displayed for the user to click on.
+   * If a customLoginButton() function is defined by your app, it will be called
+   * with 'true' passed in to indicate the button should be added. Otherwise, it
+   * will insert a textual login link at the top of the page. If defined, your
+   * showCustomLoginButton should call challengeForAuth() when clicked.
+   */
   showLoginButton() {
     if (typeof window.showCustomLoginButton === "function") {
       window.showCustomLoginButton(true);
@@ -136,11 +156,13 @@ class OneDriveAuth {
     loginText.innerText = "[sign in]";
     document.body.insertBefore(loginText, document.body.children[0]);
   }
-
-// called with the login button created by showLoginButton() needs to be
-// removed. if a customLoginButton() function is defined by your app, it will
-// be called with 'false' passed in to indicate the button should be removed.
-// otherwise it will remove the textual link that showLoginButton() created.
+  
+  /**
+   * Called with the login button created by showLoginButton() needs to be
+   * removed. If a customLoginButton() function is defined by your app, it will
+   * be called with 'false' passed in to indicate the button should be removed.
+   * Otherwise it will remove the textual link that showLoginButton() created.
+   */
   removeLoginButton() {
     if (typeof window.showCustomLoginButton === "function") {
       window.showCustomLoginButton(false);
@@ -153,6 +175,10 @@ class OneDriveAuth {
     }
   }
   
+  /**
+   * Send user to the Microsoft Account OAuth2.0 page to authorize the app
+   * and return the user to your redirectUri.
+   */
   challengeForAuth() {
     var appInfo = this.appInfo;
     var url =
